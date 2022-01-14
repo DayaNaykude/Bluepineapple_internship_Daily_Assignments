51ae12f0d6b74cac9e98acaec23b8f505859e918
Dayanand <dayanandnaykude2303@gmail.com>
2022-01-14 20:38:48 +0530

    updated

--- 10-01-2022/Assignment_4.cpp
+++ 10-01-2022/Assignment_4.cpp
@@ -4,228 +4,49 @@
 #include <string.h>
 
 using namespace std;
+const string oneToNinteen[] = {"", "One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ",
+							   "Nine ", "Ten ", "Eleven ", "Twelve ", "Thirteen ", "Fourteen ", "Fifteen ",
+							   "Sixteen ", "Seventeen ", "Eighteen ", "Ninteen "};
 
-string OneTwoDigit(int n)
+const string Tens[] = {"", "", "Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ", "Ninty "};
+
+string oneTwoDigit(int n, string outputWord)
 {
-	string outputWord;
-	int num;
-	num = (n / 10) * 10;
-	if (num > 19)
-	{
-		switch (num)
-		{
-		case 20:
-		{
-			outputWord += "Twenty ";
-			break;
-		}
-		case 30:
-		{
-			outputWord += "Thirty ";
-			break;
-		}
-		case 40:
-		{
-			outputWord += "Forty ";
-			break;
-		}
-		case 50:
-		{
-			outputWord += "Fifty ";
-			break;
-		}
-		case 60:
-		{
-			outputWord += "Sixty ";
-			break;
-		}
-		case 70:
-		{
-			outputWord += "Seventy ";
-			break;
-		}
-		case 80:
-		{
-			outputWord += "eighty ";
-			break;
-		}
-		case 90:
-		{
-			outputWord += "Ninty ";
-			break;
-		}
-		}
-	}
-	if (n > 9 && n < 20)
-	{
-		switch (n)
-		{
-		case 10:
-		{
-			outputWord += "Ten ";
-			break;
-		}
-		case 11:
-		{
-			outputWord += "Eleven ";
-			break;
-		}
-		case 12:
-		{
-			outputWord += "Twelve ";
-			break;
-		}
-		case 13:
-		{
-			outputWord += "Thirteen ";
-			break;
-		}
-		case 14:
-		{
-			outputWord += "Forteen ";
-			break;
-		}
-		case 15:
-		{
-			outputWord += "Fifteen ";
-			break;
-		}
-		case 16:
-		{
-			outputWord += "Sixteen ";
-			break;
-		}
-		case 17:
-		{
-			outputWord += "Seventeen ";
-			break;
-		}
-		case 18:
-		{
-			outputWord += "Eighteen ";
-			break;
-		}
-		case 19:
-		{
-			outputWord += "Ninteen ";
-			break;
-		}
-		}
-		return outputWord;
-	}
-	switch (n % 10)
-	{
-	case 1:
-	{
-		outputWord += "One ";
-		break;
-	}
-	case 2:
-	{
-		outputWord += "Two ";
-		break;
-	}
-	case 3:
-	{
-		outputWord += "Three ";
-		break;
-	}
-	case 4:
-	{
-		outputWord += "Four ";
-		break;
-	}
-	case 5:
-	{
-		outputWord += "Five ";
-		break;
-	}
-	case 6:
-	{
-		outputWord += "Six ";
-		break;
-	}
-	case 7:
-	{
-		outputWord += "Seven ";
-		break;
-	}
-	case 8:
-	{
-		outputWord += "Eight ";
-		break;
-	}
-	case 9:
-	{
-		outputWord += "Nine ";
-		break;
-	}
-	}
 
-	return outputWord;
-}
-
-string numberToWords(long int n)
-{
-	string output;
-	//for zero
 	if (n == 0)
 	{
-		return "zero";
-	}
-	//for two digit
-	if (n < 100)
-	{
-		output = OneTwoDigit(n);
-		return output;
+		return "";
 	}
 
-	//for three digit and onwards
-	if ((n / 100) < 10)
+	if (n > 19)
 	{
-		output = OneTwoDigit((n / 100)) + "Hundred " + OneTwoDigit((n % 100));
+		return Tens[n / 10] + oneToNinteen[n % 10] + outputWord;
 	}
-	//for four-five digit
-	else if ((n / 1000) < 100)
+	else
 	{
-		if (((n % 1000) / 100) == 0)
-		{
-			output = OneTwoDigit((n / 1000)) + "Thousand " + OneTwoDigit((n % 100));
-		}
-		else
-		{
-			output = OneTwoDigit((n / 1000)) + "Thousand " + OneTwoDigit(((n % 1000) / 100)) + "Hundred " + OneTwoDigit((n % 100));
-		}
+		return oneToNinteen[n] + outputWord;
 	}
-	//for six-seven digit
-	else if ((n / 100000) < 100)
+}
+
+string numberToWords(long long int n)
+{
+	string finalOutput;
+
+	finalOutput = oneTwoDigit((n % 100), "");
+
+	finalOutput = oneTwoDigit((n / 100) % 10, "Hundred ") + finalOutput;
+	finalOutput = oneTwoDigit((n / 1000) % 100, "Thousand ") + finalOutput;
+	finalOutput = oneTwoDigit((n / 100000) % 100, "Lakh ") + finalOutput;
+	finalOutput = oneTwoDigit((n / 10000000) % 100, "Crore ") + finalOutput;
+
+	if (n / 1000000000 >= 1)
 	{
-		if ((n % 100000) / 1000 == 0 && ((n % 1000) / 100) == 0)
-		{
-			output = OneTwoDigit((n / 100000)) + "Lakh " + OneTwoDigit((n % 100));
-		}
-		else if ((n % 100000) / 1000 == 0)
-		{
-			output = OneTwoDigit((n / 100000)) + "Lakh " + OneTwoDigit(((n % 1000) / 100)) + "Hundred " + OneTwoDigit((n % 100));
-		}
-		else if (((n % 1000) / 100) == 0)
-		{
-			output = OneTwoDigit((n / 100000)) + "Lakh " + OneTwoDigit((n % 100000) / 1000) + "Thousand " +
-					 OneTwoDigit((n % 100));
-		}
-		else
-		{
-			output = OneTwoDigit((n / 100000)) + "Lakh " + OneTwoDigit((n % 100000) / 1000) + "Thousand " +
-					 OneTwoDigit(((n % 1000) / 100)) + "Hundred " + OneTwoDigit((n % 100));
-		}
-	}
-	//for eight digit
-	else if ((n / 10000000) < 100)
-	{
-		output = OneTwoDigit((n / 10000000)) + "Crore ";
+		finalOutput = numberToWords(n / 1000000000) + "Arab " + finalOutput;
 	}
 
-	return output;
+	//finalOutput = oneTwoDigit((n / 1000000000) % 100, "Arab ") + finalOutput;
+
+	return finalOutput;
 }
 
 int main()
@@ -234,17 +55,11 @@ int main()
 	int permission;
 	do
 	{
-		long int n;
+		long long int n;
 
 	input:
-		cout << "Enter the Number upto 1 crore" << endl;
+		cout << "Enter the Number: " << endl;
 		cin >> n;
-
-		if (n > 10000000)
-		{
-			cout << "number is greter then 1 crore" << endl;
-			goto input;
-		}
 
 		cout << endl
 			 << numberToWords(n) << endl
